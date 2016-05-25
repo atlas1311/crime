2fc488221f14bc5f38c5be44a974df41c75b98ce
atlas1311 <benjamin.bettin@gmail.com>
2016-05-25 15:37:15 -0700

    Add contour map & initial plot
    Added the Google map and set the initial geom for the map

--- LAcrime.R
+++ LAcrime.R
@@ -2,5 +2,37 @@
 
 # Dependencies
 
+library(ggmap)
+library(data.table)
+library(reshape2)
+library(ggplot2)
+# D. Kahle and H. Wickham. ggmap: Spatial Visualization with ggplot2. The R Journal, 5(1),
+# 144-161. URL http://journal.r-project.org/archive/2013-1/kahle-wickham.pdf
+
 setwd("~/crime")
-crimeRaw <- read.table(file = "LA_Crimes_2012-2015.csv", header = TRUE, sep = ",")
+
+# Load and clean the data
+crimeRaw <- read.csv(file = "Crimes_2012-2015.csv", header = TRUE, sep = ",")
+crimeLA <- data.frame(crimeRaw, colsplit(crimeRaw$Location.1, pattern = "\\,", names = c("Lat", "Long")))
+crimeLA$Lat <- as.numeric(sub(pattern = "\\(", replacement = "", x = crimeLA$Lat))
+crimeLA$Long <- as.numeric(sub(pattern = "\\)", replacement = "", x = crimeLA$Long))
+
+
+# Load the base map
+LAbase <- get_map(location = c(-118.3308, 33.9931), zoom = "auto", maptype = "roadmap", 
+                  source = "google")
+LAmap <- ggmap(LAbase, fullpage = TRUE)
+
+map1 <- ggmap(LAbase, extent = "panel") + 
+        geom_density2d(data = crimeLA, aes(x = Long, y = Lat), size = 0.2) +
+        stat_density2d(data = crimeLA, aes(x = Long, y = Lat, fill = ..level.., alpha = ..level..),
+        size = 2, n = 4, geom = "polygon") +
+        scale_fill_gradient(low = "green", high = "red") +
+        scale_alpha(range = c(0, 1), guide = FALSE)
+map1
+
+
+
+
+
+
