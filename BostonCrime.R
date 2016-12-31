3573148eba2e4fb7b95bbde045f605cd006daec3
bbettin <benjamin.bettin@gmail.com>
2016-12-30 16:25:27 -0800

    Update maps to include gridlines

--- BostonCrime.R
+++ BostonCrime.R
@@ -19,7 +19,7 @@ setwd("~/crime")
 # read in the data
 crimeRaw <- read.csv(file = "BostonCrime.csv", header = TRUE, sep = ",")
 
-# split off the Lat/Long coordinates
+# split off the Lat/Long coordinates from full variable
 crimeBoston <- data.frame(crimeRaw, colsplit(crimeRaw$Location, pattern = "\\,", names = c("Lat", "Long")))
 crimeBoston$Lat <- as.numeric(sub(pattern = "\\(", replacement = "", x = crimeBoston$Lat))
 crimeBoston$Long <- as.numeric(sub(pattern = "\\)", replacement = "", x = crimeBoston$Long))
@@ -28,11 +28,14 @@ str(crimeBoston)
 # Fix the date formats
 crimeBoston$FROMDATE <- as.character(crimeBoston$FROMDATE)
 crimeBoston$DATE <- as.Date(crimeBoston$FROMDATE, format = "%m/%d/%Y %I:%M:%S %p")
+
+# Change time to 24-hour clock
+
+
+# Add month, week, day, hour formats
 crimeBoston$MONTH <- as.Date(cut(crimeBoston$DATE, breaks = "month"))
 crimeBoston$WEEK <- as.Date(cut(crimeBoston$DATE, breaks = "week", start.on.monday = FALSE))
 crimeBoston$DAY <- as.Date(cut(crimeBoston$DATE, breaks = "day"))
-crimeBoston$HOUR <- as.Date(cut(crimeBoston$DATE, breaks = "hour"))
-
 
 # Explore crime types
 unique(crimeBoston$INCIDENT_TYPE_DESCRIPTION)
@@ -56,7 +59,7 @@ violenceMonth <- as.data.frame(table(violence$MONTH))
 colnames(violenceMonth) <- c("Date", "Total")
 violenceMonth$Date <- as.character(violenceMonth$Date)
 violenceMonth$Date <- as.Date(violenceMonth$Date, format = "%Y-%m-%d")
-# Drop the last observation...something is wrong with it
+# Drop the last observation, as it appears to be an outlier or and incomplete observation.
 violenceMonth[38,] <- NA
 
 sex <- subset(crimeBoston, INCIDENT_TYPE_DESCRIPTION %in% sexCrimes)
@@ -81,9 +84,9 @@ crimeMonth <- ggplot(violenceMonth, aes(x = Date, y = Total)) +
   geom_line(position = "identity", aes(group = 1)) +
   labs(title = "Violent Crime in Boston (JUL 2012 - JUL 2015)", 
        x = "Month", y = "Violent Crime by Month") +
-  stat_smooth(method = "lm", se = TRUE, fill = "black", colour = "black", aes(group = 1)) + 
-  scale_x_date(date_labels = "%Y %b")
-# Need to fix x-axis scale. Change trendline color. Add "," on y-axis. 
+  stat_smooth(method = "lm", se = TRUE, fill = "blue", colour = "red", aes(group = 1)) + 
+  scale_x_date(date_labels = "%Y %b", date_breaks = "3 months", date_minor_breaks = "1 month") +
+  theme(plot.title = element_text(hjust = 0.5))
 crimeMonth
 
 # Sex crimes
@@ -91,9 +94,9 @@ SexCrimeMonth <- ggplot(sexMonth, aes(x = Date, y = Total)) +
   geom_line(position = "identity", aes(group = 1)) +
   labs(title = "Sex Crimes in Boston (JUL 2012 - JUL 2015)", 
        x = "Month", y = "Sex Crimes by Month") +
-  stat_smooth(method = "lm", se = TRUE, fill = "black", colour = "black", aes(group = 1)) + 
-  scale_x_date(date_labels = "%Y %b")
-# Need to fix x-axis scale. Change trendline color. Add "," on y-axis. 
+  stat_smooth(method = "lm", se = TRUE, fill = "blue", colour = "red", aes(group = 1)) + 
+  scale_x_date(date_labels = "%Y %b", date_breaks = "3 months", date_minor_breaks = "1 month") +
+  theme(plot.title =  element_text(hjust = 0.5))
 SexCrimeMonth
 
 # Property Crimes
@@ -101,46 +104,56 @@ propertyCrimeMonth <- ggplot(propertyMonth, aes(x = Date, y = Total)) +
   geom_line(position = "identity", aes(group = 1)) +
   labs(title = "Property Crimes in Boston (JUL 2012 - JUN 2015)", 
        x = "Month", y = "Property Crimes by Month") +
-  stat_smooth(method = "lm", se = TRUE, fill = "black", colour = "black", aes(group = 1)) + 
-  scale_x_date(date_labels = "%Y %b")
-# Need to fix x-axis scale. Change trendline color. Add "," on y-axis. 
+  stat_smooth(method = "lm", se = TRUE, fill = "blue", colour = "red", aes(group = 1)) + 
+  scale_x_date(date_labels = "%Y %b", date_breaks = "3 months", date_minor_breaks = "1 month") +
+  theme(plot.title = element_text(hjust = 0.5))
 propertyCrimeMonth
 
 
 # Start making some heat maps
 # Create base map for Boston
-BostonBase <- get_map(location = c(-71.057083, 42.361145), zoom = 12, maptype = "roadmap", 
+BostonBase <- get_map(location = c(-71.075, 42.34), zoom = 12, maptype = "roadmap", 
                   source = "google")
 BostonMap <- ggmap(BostonBase, fullpage = TRUE)
 
 # violent crime maps
-map1 <- ggmap(BostonBase, extent = "panel") + 
+map1 <- ggmap(BostonBase, extent = "panel", legend = "topleft") + 
         geom_density2d(data = violence, aes(x = Long, y = Lat), size = 0.3) +
         stat_density2d(data = violence, aes(x = Long, y = Lat, fill = ..level.., alpha = ..level..),
               size = 0.01, n = 50, geom = "polygon") +
         scale_fill_gradient(low = "green", high = "red") +
         scale_alpha(range = c(0, 0.25), guide = FALSE) +
-        labs(title = "Violent Crime in Boston 2012-2015", x = "Longitude", y = "Latitude") 
+        labs(title = "Violent Crime in Boston 2012-2015", x = "Longitude", y = "Latitude",
+             fill = "Crime \nDensity") +
+        theme(plot.title = element_text(hjust = 0.5)) +
+        geom_vline(xintercept = seq(-71.175, -70.00, by = .025), size = 0.25) +
+        geom_hline(yintercept = seq(42.25, 42.40, by = .025), size = 0.25)
 map1
 
 # Sex crime maps
-map2 <- ggmap(BostonBase, extent = "panel") +
+map2 <- ggmap(BostonBase, extent = "panel", legend = "topleft") +
         geom_density2d(data = sex, aes(x = Long, y = Lat), size = 0.3) +
         stat_density_2d(data = sex, aes(x = Long, y = Lat, fill = ..level.., alpha = ..level..),
               size = 0.01, n = 50, geom = "polygon") +
         scale_fill_gradient(low = "green", high = "red") +
         scale_alpha(range = c(0, 0.25), guide = FALSE) +
-        labs(title = "Sex Crimes in Boston 2012-2015", x = "Longitude", y = "Latitude")
+        labs(title = "Sex Crimes in Boston 2012-2015", x = "Longitude", y = "Latitude",
+              fill = "Crime \nDensity") +
+        geom_vline(xintercept = seq(-71.175, -70.00, by = .025), size = 0.25) +
+        geom_hline(yintercept = seq(42.25, 42.40, by = .025), size = 0.25)
 map2
 
 # Property crime maps
-map3 <- ggmap(BostonBase, extent = "panel") +
+map3 <- ggmap(BostonBase, extent = "panel", legend = "topleft") +
         geom_density2d(data = sex, aes(x = Long, y = Lat), size = 0.3) +
         stat_density_2d(data = sex, aes(x = Long, y = Lat, fill = ..level.., alpha = ..level..),
               size = 0.01, n = 50, geom = "polygon") +
         scale_fill_gradient(low = "green", high = "red") +
         scale_alpha(range = c(0, 0.25), guide = FALSE) +
-        labs(title = "Property Crimes in Boston 2012-2015", x = "Longitude",  y = "Latitude")
+        labs(title = "Property Crimes in Boston 2012-2015", x = "Longitude",  y = "Latitude",
+             fill = "Crime \nDensity") +
+        geom_vline(xintercept = seq(-71.175, -70.00, by = .025), size = 0.25) +
+        geom_hline(yintercept = seq(42.25, 42.40, by = .025), size = 0.25)
 map3
 
 # Hourly plots of the crime variables
